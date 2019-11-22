60a4b568fdaf602618b665251c7556123909cdea
Martin <martinbellec@gmail.com>
2019-11-22 21:29:16 +0100

    Ajouts des fonctions à R-fonction

--- fonctions.c
+++ fonctions.c
@@ -60,15 +60,18 @@ void detecterType(char type[], char parametre1[], char parametre2[], char parame
   }
   else if(strcmp(type, "DIV") == 0){
     printf("DIV");
+    R_fonction(26, parametre3, parametre1, parametre2);
   }
   else if(strcmp(type, "J") == 0){
     printf("J");
+
   }
   else if(strcmp(type, "JAL") == 0){
     printf("JAL");
   }
   else if(strcmp(type, "JR") == 0){
     printf("JR");
+    R_fonction(8, parametre3, parametre1, parametre2);
   }
   else if(strcmp(type, "LUI") == 0){
     printf("LUI");
@@ -78,18 +81,23 @@ void detecterType(char type[], char parametre1[], char parametre2[], char parame
   }
   else if(strcmp(type, "MFHI") == 0){
     printf("MFHI");
+    R_fonction(16, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "MFLO") == 0){
     printf("MFLO");
+    R_fonction(18, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "MULT") == 0){
     printf("MULT");
+    R_fonction(24, parametre3, parametre1, parametre2);
   }
   else if(strcmp(type, "NOP") == 0){
     printf("NOP");
+    R_fonction(0, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "OR") == 0){
     printf("OR");
+    R_fonction(37, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "ROTR") == 0){
     printf("ROTR");
@@ -99,21 +107,25 @@ void detecterType(char type[], char parametre1[], char parametre2[], char parame
   }
   else if(strcmp(type, "SLT") == 0){
     printf("SLT");
+    R_fonction(42, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "SRL") == 0){
     printf("SRL");
   }
   else if(strcmp(type, "SUB") == 0){
     printf("SUB");
+    R_fonction(34, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "SW") == 0){
     printf("SW");
   }
   else if(strcmp(type, "SYSCALL") == 0){
     printf("SYSCALL");
+    R_fonction(12, parametre1, parametre2, parametre3);
   }
   else if(strcmp(type, "XOR") == 0){
     printf("XOR");
+    R_fonction(38, parametre1, parametre2, parametre3);
   }
 }
 
@@ -137,12 +149,12 @@ int ADDI(char type[], char parametre1[], char parametre2[], char parametre3[]) {
   return resultat;
 }
 
-int R_fonction(int type, char parametre1[], char parametre2[], char parametre3[]) {
+int R_fonction(int type, char rd[], char rs[], char rt[]) {
   int resultat = type;
   resultat += 0 >> 6;
-  resultat += atoi(&parametre1[1]) << 11;
-  resultat += atoi(&parametre3[1]) << 16;
-  resultat += atoi(&parametre2[1]) << 21;
+  resultat += atoi(&rd[1]) << 11;
+  resultat += atoi(&rt[1]) << 16;
+  resultat += atoi(&rs[1]) << 21;
   printf("%08x\n", resultat);
   return resultat;
 }
--- fonctions.h
+++ fonctions.h
@@ -3,7 +3,6 @@
 #include <math.h>
 #include <string.h>
 
-
 void lireCommande(char commande[], char type[], char parametre1[], char parametre2[], char parametre3[]);
 void detecterType(char type[], char parametre1[], char parametre2[], char parametre3[]);
 int ADD(char type[], char parametre1[], char parametre2[], char parametre3[]);
