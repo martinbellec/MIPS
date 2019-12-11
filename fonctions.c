bebb91f6e97a65b47c8893ae5b3f2ccd659699b1
guigui3899 <guilhem99.rolland@gmail.com>
2019-12-11 11:02:05 +0100

    fn I dans fn.c

--- fonctions.c
+++ fonctions.c
@@ -247,3 +247,11 @@ int J_fonction(int type, char target[]) {
   resultat += (atoi(&target[0]) & 67108863);
   return resultat;
 }
+
+int I_fonction(int type, char parametre1[], char parametre2[], char parametre3[]){
+  int resultat = type << 26;
+  resultat += atoi(&parametre3[0]);
+  resultat += atoi(&parametre1[1]) << 16;
+  resultat += atoi(&parametre2[1]) << 21;
+  return resultat;
+}
--- guilhemfn.c
+++ guilhemfn.c
@@ -1,13 +1,3 @@
 #include "register.h"
 #include "guilhemfn.h"
 #include "memory.h"
-
-
-int I_fonction(int type, char parametre1[], char parametre2[], char parametre3[]){
-  int resultat = type << 26;
-  resultat += atoi(&parametre3[0]);
-  resultat += atoi(&parametre1[1]) << 16;
-  resultat += atoi(&parametre2[1]) << 21;
-  /*printf("%08x\n", resultat);*/
-  return resultat;
-}
